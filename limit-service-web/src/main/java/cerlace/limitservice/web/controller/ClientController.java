6909c01d9b4324598aa72d08927a019bd0540f0b
Cerlace <124503215+Cerlace@users.noreply.github.com>
2025-04-15 09:08:50 +0300

    SwaggerConfig and controllers docs

--- limit-service-web/src/main/java/cerlace/limitservice/web/controller/ClientController.java
+++ limit-service-web/src/main/java/cerlace/limitservice/web/controller/ClientController.java
@@ -4,6 +4,10 @@ import cerlace.limitservice.core.dto.SpendLimitCreateRequest;
 import cerlace.limitservice.core.dto.SpendLimitResponse;
 import cerlace.limitservice.core.dto.TransactionResponse;
 import cerlace.limitservice.core.service.ClientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +19,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "Клиентский модуль",
+        description = """
+                API для управлением лимитами на расходные операции
+                и получения транзакций, превысивших лимиты.""")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/client")
@@ -22,17 +30,36 @@ public class ClientController {
 
     private final ClientService clientService;
 
-    @PostMapping(value ="/set_limit", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(
+            summary = "Установить расходный лимит",
+            description = """
+                    Обрабатывает запрос на установку нового лимита
+                    по заданной категории, и сохраняет его в базу данных."""
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Лимит успешно установлен"),
+            @ApiResponse(responseCode = "400", description = "Ошибка входных данных"),
+            @ApiResponse(responseCode = "500", description = "Внутрення ошибка сервера")
+    })
+    @PostMapping(value = "/set_limit", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<SpendLimitResponse> setSpendLimit(@RequestBody SpendLimitCreateRequest request) {
         SpendLimitResponse createdLimit = clientService.setSpendLimit(request);
         return ResponseEntity.ok(createdLimit);
     }
 
+    @Operation(
+            summary = "Получить все лимиты",
+            description = "Получает все существующие расходные лимиты"
+    )
     @GetMapping(value = "/get_limits", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<SpendLimitResponse> getAllSpendLimits() {
         return clientService.getAllSpendLimits();
     }
 
+    @Operation(
+            summary = "Получить все лимитные транзакции",
+            description = "Получает все транзакции, которые превысили установленный расходный лимит"
+    )
     @GetMapping(value = "/get_transactions", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<TransactionResponse> getLimitExceededTransactions() {
         return clientService.getLimitExceededTransactions();
--- limit-service-web/src/main/java/cerlace/limitservice/web/controller/TransactionController.java
+++ limit-service-web/src/main/java/cerlace/limitservice/web/controller/TransactionController.java
@@ -3,6 +3,10 @@ package cerlace.limitservice.web.controller;
 import cerlace.limitservice.core.dto.TransactionCreateRequest;
 import cerlace.limitservice.core.dto.TransactionResponse;
 import cerlace.limitservice.core.service.TransactionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Модуль транзакций",
+        description = "API для обработки входящих транзакций")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/transaction")
@@ -18,7 +24,19 @@ public class TransactionController {
 
     private final TransactionService transactionService;
 
-    @PostMapping(value ="/add", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(
+            summary = "Сохранить транзакцию",
+            description = """
+                    Обрабатывает запрос со входящей транзакцией:
+                    проверяет транзакцию на превышение существующего лимита по категории,
+                    и сохраняет ее в базу данных."""
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Транзакция успешно сохранена"),
+            @ApiResponse(responseCode = "400", description = "Ошибка входных данных"),
+            @ApiResponse(responseCode = "500", description = "Внутрення ошибка сервера")
+    })
+    @PostMapping(value = "/add", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<TransactionResponse> saveTransaction(@RequestBody TransactionCreateRequest request) {
         TransactionResponse savedTransaction = transactionService.saveTransaction(request);
         return ResponseEntity.ok(savedTransaction);
