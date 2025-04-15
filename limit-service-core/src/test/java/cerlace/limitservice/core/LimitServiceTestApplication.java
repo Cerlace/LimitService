e303390eb5c045ebc2445f1a317415231c0131ae
Cerlace <124503215+Cerlace@users.noreply.github.com>
2025-04-15 03:14:45 +0300

    Integration tests properties and dependency

---
+++ limit-service-core/src/test/java/cerlace/limitservice/core/LimitServiceTestApplication.java
@@ -0,0 +1,11 @@
+package cerlace.limitservice.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitServiceTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LimitServiceTestApplication.class, args);
+    }
+}
